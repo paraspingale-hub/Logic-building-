f96ffdfd8c04f2c0c461803f2bc2852339727852
Paras Pingale <paraspingales@gmail.com>
2026-08-19 22:24:07 +0530

    Java codes

---
+++ Java/program24.java
@@ -0,0 +1,17 @@
+class Program24
+{
+    public static void Display()
+    {
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+    }
+
+    public static void main(String A[])
+    {
+        Display();
+
+        return;
+    }
+}
---
+++ Java/program25.java
@@ -0,0 +1,17 @@
+class Program25
+{
+    public static void Display()
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt <= 4; iCnt++)
+        {
+            System.out.println("Jay Ganesh...");
+        }
+    }
+
+    public static void main(String A[])
+    {
+        Display();
+    }
+}
