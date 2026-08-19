52806e48805db6857511cf50d6575a737d42c5a2
Paras Pingale <paraspingales@gmail.com>
2026-08-19 22:25:20 +0530

    Java codes

--- Java/program27.java
+++ Java/program27.java
@@ -1,4 +1,35 @@
-public class
- program20 {
-    
+import java.util.Scanner;
+
+class Program27
+{
+    public static void Display(int iFrequency)
+    {
+        int iCnt = 0;
+
+        // Filter
+        if(iFrequency < 0)
+        {
+            System.out.println("Invalid input");
+            return;
+        }
+
+        for(iCnt = 1; iCnt <= iFrequency; iCnt++)
+        {
+            System.out.println("Jay Ganesh...");
+        }
+    }
+
+    public static void main(String A[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int iCount = 0;
+
+        System.out.println("Enter the frequency : ");
+        iCount = sc.nextInt();
+
+        Display(iCount);
+
+        sc.close();
+    }
 }
--- Java/program30.java
+++ Java/program30.java
@@ -1,4 +1,22 @@
-public class
- program20 {
-    
+class Program30
+{
+    public static void Display()
+    {
+        int iCnt = 0;
+
+        iCnt = 1;
+
+        while(iCnt <= 5)
+        {
+            System.out.print(iCnt + "\t");
+            iCnt++;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String A[])
+    {
+        Display();
+    }
 }
