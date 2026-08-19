6d29ba0cb167b4e89748d94c355b0b83859f8085
Paras Pingale <paraspingale1@gmail.com>
2026-08-19 22:25:20 +0530

    Java codes

--- Java/program28.java
+++ Java/program28.java
@@ -1,4 +1,18 @@
-public class
- program20 {
-    
+class Program28
+{
+    public static void Display()
+    {
+        System.out.print("1\t");
+        System.out.print("2\t");
+        System.out.print("3\t");
+        System.out.print("4\t");
+        System.out.print("5\t");
+
+        System.out.println();
+    }
+
+    public static void main(String A[])
+    {
+        Display();
+    }
 }
--- Java/program29.java
+++ Java/program29.java
@@ -1,4 +1,19 @@
-public class
- program20 {
-    
+class Program29
+{
+    public static void Display()
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt <= 5; iCnt++)
+        {
+            System.out.print(iCnt + "\t");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String A[])
+    {
+        Display();
+    }
 }
