8b2b0093c302adac2f83839f5c361e25d1262771
Paras Pingale <paraspingales@gmail.com>
2026-09-20 20:57:11 +0530

    java design codes

--- Java/program39.java
+++ Java/program39.java
@@ -1,15 +1,37 @@
 class program38
 {
-    public void  DisplayFactors()
+    public boolean  CheckPrime(int iNo)
 {
-    
+    int iCnt = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+for(iCnt = 2; iCnt <= (iNo / 2); iCnt++)
+    {
+        if((iNo % iCnt) == 0)
+        {
+            break;          // Optimization
+        }
+    }
+
+    if(iCnt > (iNo/2))     // No factors
+    {
+        return true;
+    }
+    else                    // Atleast one factor
+    {
+        return false;
+    }
     
 }
 
     public static void main(String A[])
     {
+        int iNo = 90;
         program38 chk = new program38();
-        chk.DisplayFactors();
+        chk.CheckPrime(iNo);
        
     }
 }
